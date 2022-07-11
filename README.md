aaaa3083beafb11dcf99db196c15a9c881a26fcd
Saif Eddine Nouma <41328983+SaifNOUMA@users.noreply.github.com>
2022-07-11 11:08:25 -0400

    Mod.  Update Readme files

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # OAS
-Optimal Audit Signature
+Optimal Signatures for Audit
 
 
 ## Prerequisites
@@ -9,5 +9,17 @@ Optimal Audit Signature
 
 ## Build and Compile
 
-Note: Coming soon after cleaning up the codes!!
+The repository contains:
+1. datagen:
+    Contains utility functions to generate a set of random log entries
+2. FIPOSA:
+    Contains the implementation of our first scheme FIne-grained Post-audit OSA (FIPOSA).
+    To build:
+        Go to FourQ_64bit_and_portable folder, and exeucte: make ``make ARCH=x64``
+3. SOCOSA:
+    Contains the implementation of our first scheme Signer-Optimal Coarse-grained OSA (SOCOSA).
+    To build:
+        Go to FourQ_64bit_and_portable folder, and exeucte: make ``make ARCH=x64``
+
+<!-- Note: Coming soon after cleaning up the codes!! -->
 
---
+++ datagen/data_gen.cpp
@@ -0,0 +1,50 @@
+
+#include <stdio.h>
+#include <iostream>
+#include <fstream>
+#include <string.h>
+#include <string>
+#include <time.h>
+#include <openssl/rand.h>
+
+#define NUM_CHUNKS 128
+
+
+using namespace std;
+
+int main (int argc, char *argv[])
+{
+    static uint8_t logdata[1000000000L];
+    ofstream    output_file;
+    double      gen_time = 0.0, store_time = 0.0;
+    clock_t     t0, t1;
+    char*       path2outfile;
+    size_t      outfile_len;
+
+
+    outfile_len = atoi(argv[1]);
+    path2outfile = argv[2];
+    output_file.open(path2outfile);
+    for (int chunk = 0 ; chunk < outfile_len ; chunk++) {
+        t0 = clock();
+        if (0 == RAND_bytes(logdata, sizeof(logdata))) {
+            printf("ERROR: Data generation failed");
+        }
+        t1 = clock();
+        gen_time += (double) (t1-t0);
+
+        t0 = clock();
+        for (size_t i = 0 ; i < sizeof(logdata) ; i++) {
+            output_file << (int) logdata[i] % 10;
+        }
+        t1 = clock();
+        store_time += (double) (t1-t0);
+    }
+    output_file.close();
+
+    printf("DEBUG: Data generation passed successfully in %.2f s.\n",       ((double) (gen_time)) / (CLOCKS_PER_SEC) );
+    printf("DEBUG: Storing audit data passed successfully in %.2f s.\n",    ((double) (store_time)) / (CLOCKS_PER_SEC) );
+
+
+   return 0; 
+}
